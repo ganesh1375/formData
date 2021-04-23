d1cbb054beaf23d9c7a69f4516d6dfa3ac533b62
Ganesh Shivarathri <ganeshshivarathri22@gmail.com>
2021-04-23 14:22:47 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {ReactiveFormsModule} from '@angular/forms'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormControl,FormBuilder} from '@angular/forms';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(private fb:FormBuilder, private _http:HttpClient) { }
+   registrationFrom:any;
+
+  ngOnInit(): void {
+
+    // this.registrationFrom=this.fb({
+      
+    // })
+  }
+  onSubmit(userForm:any)
+  {
+    //console.log(userForm);
+    this.enroll(userForm).subscribe((data: any)=>console.log("success",data));
+
+  }
+
+  enroll(user:any):Observable<any>
+  {
+    return this._http.post('http://localhost:4000/form',user);
+  }
+
+
+
+}
